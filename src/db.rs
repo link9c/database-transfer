34d17ac0a9247295a17bdaf8704c9b53ad046506
Your Name <you@example.com>
2022-04-03 22:52:44 +0800

    0413

--- src/db.rs
+++ src/db.rs
@@ -6,10 +6,27 @@ use mysql_async::{
 use tiberius::{AuthMethod, Client, Config};
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
-
+#[derive(Debug, Clone,Copy)]
 pub enum Direct {
     FROM,
     TO,
+}
+
+impl Default for Direct {
+    fn default() -> Self {
+        Direct::FROM
+    }
+}
+
+impl Direct {
+
+    pub fn toggle(&mut self)->Self{
+        match self{
+            Direct::FROM => Self::TO,
+            Direct::TO => Self::FROM,
+        }
+    }
+    
 }
 
 pub enum SQLClient {
@@ -62,14 +79,13 @@ impl DatabaseConfig {
         }
     }
 }
-#[derive(Debug, Clone,Default)]
+#[derive(Debug, Clone, Default)]
 pub struct DatabaseMeta {
     from_db: DatabaseConfig,
     to_db: DatabaseConfig,
 }
 
 impl DatabaseMeta {
-
     pub fn initial() -> Self {
         Self {
             from_db: DatabaseConfig::from_config(Direct::FROM),
@@ -154,6 +170,128 @@ impl DatabaseMeta {
                         r.to_string()
                     })
                     .collect::<Vec<String>>();
+
+                res
+            }
+        };
+        Ok(res)
+    }
+
+    pub async fn show_tables(
+        self,
+        direct: Direct,
+        ddb: String,
+    ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+        let client = self.client(direct).await?;
+        let res = match client {
+            SQLClient::Mysql((mut c, p)) => {
+                let sql_str = format!(
+                    "select table_name from information_schema.tables where table_schema='{}' and table_type='base table';"
+                    ,ddb
+                );
+                let mut result = c.query_iter(sql_str).await?;
+                let res = result.collect::<String>().await?;
+                drop(c);
+                p.disconnect().await?;
+                res
+            }
+            SQLClient::Mssql(mut c) => {
+                let row = c
+                    .simple_query(" SELECT  name FROM SysDatabases")
+                    .await?
+                    .into_results()
+                    .await?;
+
+                let res = row[0]
+                    .iter()
+                    .map(|x| {
+                        let r: &str = x.get(0).unwrap();
+                        r.to_string()
+                    })
+                    .collect::<Vec<String>>();
+
+                res
+            }
+        };
+        Ok(res)
+    }
+
+    pub async fn table_detail(
+        self,
+        direct: Direct,
+        ddb: String,
+        table: String,
+    ) -> Result<Vec<(String, String, String, String)>, Box<dyn std::error::Error>> {
+        let client = self.client(direct).await?;
+        let res = match client {
+            SQLClient::Mysql((mut c, p)) => {
+                let sql_str = format!(
+                    "SELECT
+                    COLUMN_NAME,
+                    COLUMN_DEFAULT,
+                    DATA_TYPE,
+                    CHARACTER_MAXIMUM_LENGTH,
+                    COLUMN_COMMENT
+                FROM
+                    information_schema.`COLUMNS` 
+                WHERE
+                    TABLE_SCHEMA = '{}' 
+                    AND table_name = '{}' 
+                ORDER BY
+                    TABLE_NAME,
+                    ORDINAL_POSITION;",
+                    ddb, table
+                );
+                let mut result = c.query_iter(sql_str).await?;
+                let res = result.collect::<(String, String, String, String)>().await?;
+                drop(c);
+                p.disconnect().await?;
+                res
+            }
+            SQLClient::Mssql(mut c) => {
+                let sql_str = format!(
+                    "SELECT
+                a.name AS COLUMN_NAME,
+                isnull( e.text, '' ) AS COLUMN_DEFAULT,
+                b.name AS DATA_TYPE,
+                COLUMNPROPERTY( a.id, a.name, 'PRECISION' ) AS CHARACTER_MAXIMUM_LENGTH,
+                isnull( g.[value], '' ) AS COLUMN_COMMENT 
+            FROM
+                syscolumns a
+                LEFT JOIN systypes b ON a.xtype= b.xusertype
+                INNER JOIN sysobjects d ON a.id= d.id 
+                AND d.xtype= 'U' 
+                AND d.name<> 'dtproperties'
+                LEFT JOIN syscomments e ON a.cdefault= e.id
+                LEFT JOIN sys.extended_properties g ON a.id= g.major_id 
+                AND a.colid= g.minor_id
+                LEFT JOIN sys.extended_properties f ON d.id= f.major_id 
+                AND f.minor_id = 0 
+            WHERE
+                d.name= '{}' 
+            ORDER BY
+                a.id,
+                a.colorder",
+                    table
+                );
+                println!("{}",sql_str);
+                let row = c.simple_query(sql_str).await?.into_results().await?;
+
+                let res = row[0]
+                    .iter()
+                    .map(|x| {
+                        let r1: &str = x.get(0).unwrap();
+                        let r2: &str = x.get(0).unwrap();
+                        let r3: &str = x.get(0).unwrap();
+                        let r4: &str = x.get(0).unwrap();
+                        (
+                            r1.to_string(),
+                            r2.to_string(),
+                            r3.to_string(),
+                            r4.to_string(),
+                        )
+                    })
+                    .collect::<Vec<(String, String, String, String)>>();
 
                 res
             }
--- src/gui/ui.rs
+++ src/gui/ui.rs
@@ -1,4 +1,12 @@
-use iced::{Align, Application, Button, Checkbox, Clipboard, Column, Command, Container, Element, HorizontalAlignment, Length, PickList, Row, Scrollable, Settings, Subscription, Text, VerticalAlignment, button, checkbox, executor, futures::executor::block_on, pick_list, scrollable, window::{self, Icon}};
+use iced::{
+    button, checkbox, executor,
+    futures::executor::block_on,
+    pick_list, scrollable,
+    window::{self, Icon},
+    Align, Application, Button, Checkbox, Clipboard, Column, Command, Container, Element,
+    HorizontalAlignment, Length, PickList, Row, Scrollable, Settings, Subscription, Text,
+    VerticalAlignment,
+};
 
 use crate::gui::{icon, style};
 
@@ -24,7 +32,7 @@ pub fn render_window() -> iced::Result {
 
     MyUi::run(Settings {
         window: window::Settings {
-            size: (440, 320),
+            size: (800, 600),
             min_size: Some((200, 100)),
             max_size: None,
             resizable: true,
@@ -42,16 +50,20 @@ pub fn render_window() -> iced::Result {
 pub struct MyUi {
     // db_meta:DatabaseMeta,
     db_name: String,
-    table_list: Vec<(String, usize,bool)>,
-    check_button_list: Vec<button::State>,
+    db_name_to: String,
+    db_meta: DatabaseMeta,
+    direction: Direct,
+    table_list: Vec<(String, usize, bool)>,
+    check_button_list_left: Vec<button::State>,
+    check_button_list_right: Vec<button::State>,
     theme: Option<style::Theme>,
     init_button: button::State,
-    left_button: button::State,
-    right_button: button::State,
+    switch_button: button::State,
+    ensure_button: button::State,
     font_dec_button: button::State,
-    check_list: Vec<String>,
     pick_list_theme: pick_list::State<style::Theme>,
     scroll_left: scrollable::State,
+    scroll_right: scrollable::State,
 }
 
 // impl GetCard {
@@ -69,9 +81,11 @@ pub struct MyUi {
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    LoadConf,
+    DirectChanged,
+    LoadConf(Direct),
+
     SelectedTable((bool, usize)),
-    DbSelected(String),
+    Transfer,
     ThemeChanged(style::Theme),
 }
 
@@ -99,47 +113,102 @@ impl Application for MyUi {
             Text::new("load conf").height(Length::Shrink),
         )
         .style(self.theme.unwrap())
-        .on_press(Message::LoadConf);
+        .on_press(Message::LoadConf(self.direction));
 
         let text = Text::new(&self.db_name).height(Length::Shrink);
 
-        let table_list = self
+        let table_list_left = self
             .table_list
             .iter()
-            .filter(|x| x.2==true)
-            .zip(&mut self.check_button_list)
+            .filter(|x| x.2 == true)
+            .zip(&mut self.check_button_list_left)
             .fold(
                 Column::new().spacing(1),
-                |col, ((name, idx,enabled), but)| {
+                |col, ((name, idx, enabled), but)| {
                     col.push(
-                        Row::new()
-                            .push(
-                                Button::new(but, Text::new(name).height(Length::Fill))
-                                    .style(self.theme.unwrap())
-                                    .on_press(Message::SelectedTable((enabled.to_owned(),idx.to_owned()))),
-                            )
-                            .push(Text::new("X").vertical_alignment(VerticalAlignment::Center)),
+                        Button::new(but, Text::new(name).height(Length::Fill))
+                            .style(self.theme.unwrap())
+                            .on_press(Message::SelectedTable((enabled.to_owned(), idx.to_owned()))),
+                    )
+                },
+            );
+
+        let table_list_right = self
+            .table_list
+            .iter()
+            .filter(|x| x.2 == false)
+            .zip(&mut self.check_button_list_right)
+            .fold(
+                Column::new().spacing(1),
+                |col, ((name, idx, enabled), but)| {
+                    col.push(
+                        Button::new(but, Text::new(name).height(Length::Fill))
+                            .style(self.theme.unwrap())
+                            .on_press(Message::SelectedTable((enabled.to_owned(), idx.to_owned()))),
                     )
                 },
             );
 
         let db_table_scroll_left = Scrollable::new(&mut self.scroll_left)
-            .push(table_list)
+            .push(table_list_left)
             .width(Length::Fill)
             .height(Length::Fill);
+
+        let db_table_scroll_right = Scrollable::new(&mut self.scroll_right)
+            .push(table_list_right)
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        let middle_button_group = Column::new()
+            .push(
+                Button::new(
+                    &mut self.switch_button,
+                    Text::new("swicth").height(Length::Fill),
+                )
+                .style(self.theme.unwrap())
+                .on_press(Message::LoadConf(Direct::FROM))
+                .width(Length::Shrink)
+                .height(Length::Shrink),
+            )
+            .push(
+                Button::new(
+                    &mut self.ensure_button,
+                    Text::new("ok").height(Length::Fill),
+                )
+                .style(self.theme.unwrap())
+                .on_press(Message::Transfer)
+                .width(Length::Shrink)
+                .height(Length::Shrink),
+            )
+            .align_items(Align::Center);
+
+        let row = Row::new()
+            .push(
+                Container::new(db_table_scroll_left)
+                    .style(self.theme.unwrap())
+                    .width(Length::Fill)
+                    .height(Length::Fill),
+            )
+            .push(
+                Container::new(middle_button_group)
+                    .center_y()
+                    .width(Length::Shrink)
+                    .height(Length::Fill),
+            )
+            .push(
+                Container::new(db_table_scroll_right)
+                    .style(self.theme.unwrap())
+                    .width(Length::Fill)
+                    .height(Length::Fill),
+            )
+            .padding(5);
 
         let content = Column::new()
             // .padding(5)
             // .align_items(Alignment::c)
-            .push(
-                Container::new(init_button)
-                    .width(Length::Fill)
-                    .height(Length::Shrink)
-                    .padding(5)
-                    .style(self.theme.unwrap()),
-            )
+            .push(init_button)
             .push(text)
-            .push(db_table_scroll_left);
+            .push(row);
 
         // .push(button2);
 
@@ -153,38 +222,66 @@ impl Application for MyUi {
 
     fn update(&mut self, message: Self::Message, _: &mut Clipboard) -> Command<Self::Message> {
         match message {
-            Message::LoadConf => {
+            Message::LoadConf(mut direct) => {
                 let db_meta = DatabaseMeta::initial();
-                self.db_name = db_meta.clone().get_default_db(Direct::TO);
+                self.db_meta = db_meta.clone();
+                self.db_name = db_meta.clone().get_default_db(direct);
+                self.db_name_to = db_meta.clone().get_default_db(direct.toggle());
 
-                let table_list =
-                    block_on(async move { db_meta.clone().show_dbs(Direct::TO).await });
+                let table_list = block_on(async move {
+                    db_meta
+                        .clone()
+                        .show_tables(direct, db_meta.clone().get_default_db(direct))
+                        .await
+                });
 
                 match table_list {
                     Ok(val) => {
                         self.table_list = val
                             .iter()
                             .enumerate()
-                            .map(|(idx, x)| (x.to_owned(), idx,true))
-                            .collect::<Vec<(String, usize,bool)>>();
-                        self.check_button_list = vec![button::State::new(); val.len()];
+                            .map(|(idx, x)| (x.to_owned(), idx, true))
+                            .collect::<Vec<(String, usize, bool)>>();
+                        self.check_button_list_left = vec![button::State::new(); val.len()];
+                        self.check_button_list_right = vec![button::State::new(); val.len()];
                     }
-                    Err(_) => self.table_list = vec![("TimedOut".to_string(), 0,true)],
+                    Err(_) => self.table_list = vec![("TimedOut".to_string(), 0, true)],
                 }
             }
-            Message::DbSelected(_) => todo!(),
             Message::ThemeChanged(t) => self.theme = Some(t),
             Message::SelectedTable(b) => {
                 println!("{:?}", b);
-           
-                self.table_list[b.1].2=!b.0;
-             
-                
+
+                self.table_list[b.1].2 = !b.0;
+
                 // for each in &*checked{
                 //     println!("{}",each);
                 //     self.table_list[each.to_owned()].1 =b
                 // }
             }
+            Message::Transfer => {
+                // self.db_meta.table_detail(direct, ddb, table)
+                let res = self
+                    .table_list
+                    .iter()
+                    .filter(|x| x.2 == false).map(|x| x.0.clone()).collect::<Vec<String>>();
+
+                println!("{:?}",res);
+                    
+
+                let table_detail = block_on(async move {
+                    self.db_meta
+                        .clone()
+                        .table_detail(self.direction, self.db_name.clone(), res[0].clone())
+                        .await
+                });
+
+                println!("{:?}",table_detail);
+            }
+            Message::DirectChanged => match self.direction {
+                Direct::FROM => self.direction = Direct::TO,
+                Direct::TO => self.direction = Direct::FROM,
+            },
         }
 
         Command::none()
